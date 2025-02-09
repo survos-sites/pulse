c008daab87b156207aa1f77109cf5227cc6cca16
Tac Tacelosky <tacman@gmail.com>
2025-02-09 07:43:24 -0500

    update libraries, use WithTest()

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\Attributes\TestWith;
+use Pierstoval\SmokeTesting\FunctionalSmokeTester;
+use Pierstoval\SmokeTesting\FunctionalTestData;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FunctionalTest extends WebTestCase
+{
+    use FunctionalSmokeTester;
+
+    #[TestWith(['/api','api_entrypoint'])]
+    public function testRoute(string $url, string $route, string $method='GET'): void
+    {
+        $this->runFunctionalTest(
+            FunctionalTestData::withUrl($url)
+                ->withMethod($method)
+                ->expectRouteName($route)
+                ->appendCallableExpectation($this->assertStatusCodeLessThan500($method, $url))
+        );
+
+    }
+
+    public function assertStatusCodeLessThan500(string $method, string $url): \Closure
+    {
+        return function (KernelBrowser $browser) use ($method, $url) {
+            $statusCode = $browser->getResponse()->getStatusCode();
+            $routeName = $browser->getRequest()->attributes->get('_route', 'unknown');
+
+            static::assertLessThan(
+                500,
+                $statusCode,
+                sprintf('Request "%s %s" for %s route returned an internal error.', $method, $url, $routeName),
+            );
+        };
+    }
+}
